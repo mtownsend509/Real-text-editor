cf4cdfbe0548056fbde47f833091c0f3513a4770
mtownsend509 <mgtownsend509@gmail.com>
2023-02-23 21:55:48 -0800

    whole project moved over because it won't deploy

---
+++ src-sw.js
@@ -0,0 +1,50 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { StaleWhileRevalidate ,CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+// const cacheName = 'static-resources';
+// const matchCallback = ({ request }) => {
+//   console.log(request);
+//   return (
+//     request.destination === 'style' ||
+//     request.destination === 'script'
+//   );
+// };
+
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
